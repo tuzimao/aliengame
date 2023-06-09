7517951faea8265f0d1733cd3c1547341308fee9
tuzimao <zliu1@ualberta.ca>
2023-06-08 17:23:56 -0700

    set background set ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,23 +1,32 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_heigh))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
+            self.clock.tick(60)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
