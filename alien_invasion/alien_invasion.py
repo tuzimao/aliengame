f732269e80a33e54c3d12285b652dc013fe5394e
tuzimao <zliu1@ualberta.ca>
2023-06-09 13:17:26 -0700

    add fire bullet to the game

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -10,16 +11,24 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_heigh))
+
+        # full screen mode
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        # custom setting screen mode
+        # self.screen = pygame.display.set_mode(
+        #   (self.settings.screen_width, self.settings.screen_heigh))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
             self.check_events()
             self.ship.update()
+            self.bullets.update()
             self.update_screen()
             self.clock.tick(60)
 
@@ -39,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -46,8 +57,14 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def update_screen(self):
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
         pygame.display.flip()
 
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,3 +4,8 @@ class Settings:
         self.screen_heigh = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 3.5
+
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
