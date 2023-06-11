86f2b68b37f474edde3aa0711873a0eb83475a18
tuzimao <zliu1@ualberta.ca>
2023-06-11 16:13:21 -0700

    aliens can move

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self.check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self.update_screen()
             self.clock.tick(60)
 
@@ -99,6 +100,21 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 
 if __name__ == '__main__':
